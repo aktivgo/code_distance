f338d80f06bbe97b4cf9ed70295f31ee79f02a18
aktivgo <AktivGo01062001@gmail.com>
2021-11-24 09:11:28 +0300

    Initial commit

---
+++ code_distance/CodeDistance.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace code_distance
+{
+    public class CodeDistance
+    {
+        public List<int> GetCodeDistance(List<List<int>> H)
+        {
+            List<int> d = new List<int>();
+
+            return d;
+        }
+    }
+}
---
+++ code_distance/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace code_distance
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
