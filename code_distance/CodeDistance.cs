e5fa7f9b059434fca88c7b0be083bc9a98ad74e0
aktivgo <AktivGo01062001@gmail.com>
2021-11-24 09:40:01 +0300

    Complete task

--- code_distance/CodeDistance.cs
+++ code_distance/CodeDistance.cs
@@ -2,11 +2,27 @@
 
 namespace code_distance
 {
-    public class CodeDistance
+    public static class CodeDistance
     {
-        public List<int> GetCodeDistance(List<List<int>> H)
+        public static List<int> GetCodeDistance(List<List<int>> H)
         {
             List<int> d = new List<int>();
+
+            for (int i = 0; i < H.Count; i++)
+            {
+                for (int j = i + 1; j < H.Count; j++)
+                {
+                    int distance = 0;
+                    for (int k = 0; k < H[i].Count; k++)
+                    {
+                        if (H[i][k] != H[j][k])
+                        {
+                            distance++;
+                        }
+                    }
+                    d.Add(distance);
+                }
+            }
 
             return d;
         }
--- code_distance/Program.cs
+++ code_distance/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace code_distance
+﻿using System;
+using System.Collections.Generic;
+
+namespace code_distance
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.Write("Введите количество строк матрицы: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<List<int>> H = new List<List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                H.Add(new List<int>());
+
+                string line = Console.ReadLine();
+                foreach (var item in line)
+                {
+                    H[i].Add(int.Parse(item.ToString()));
+                }
+            }
+
+            List<int> d = CodeDistance.GetCodeDistance(H);
+            Console.Write("D = ");
+            foreach (var item in d)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
